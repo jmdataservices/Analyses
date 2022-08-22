dda3fda54073be7dad023ce973044c8c98fa0a7e
Jamie McLaughlin <37658247+jmoss84@users.noreply.github.com>
2022-08-23 00:02:47 +0100

    Broke the back of the general process and made sweeping changes to the Rmd, EDA.R and CSS approaches. Grind to complete. Added temporary HTML output to webapp project.

--- 001-AdultEarnings/EDA.R
+++ 001-AdultEarnings/EDA.R
@@ -1,7 +1,9 @@
-source("dependencies.R")
+dir <- gsub("/Analyses.*$", "/Analyses", getwd())
+
+source(paste0(dir, "/dependencies.R"))
 
 dat_adults <- read.csv(
-        "001-AdultEarnings/src/adult.data",
+        paste0(dir, "/001-AdultEarnings/src/adult.data"),
         col.names = c(
                 "Age",
                 "WorkClass",
@@ -20,11 +22,28 @@ dat_adults <- read.csv(
                 "EarningsClass"
         )
 )
-dat_test <- read.csv("001-AdultEarnings/src/adult.test", skip = 1)
+
+dat_test <- read.csv(
+        paste0(dir, "/001-AdultEarnings/src/adult.test"),
+        skip = 1
+)
 
 dat_adults <- dat_adults %>%
         mutate(
-                across(.cols = c(EarningsClass, MaritalStatus, WorkClass, Education, Occupation, RelationshipStatus, Race, Sex, NativeCountry), .fns = trimws),
+                across(
+                        .cols = c(
+                                EarningsClass,
+                                MaritalStatus,
+                                WorkClass,
+                                Education,
+                                Occupation,
+                                RelationshipStatus,
+                                Race,
+                                Sex,
+                                NativeCountry
+                        ),
+                        .fns = trimws
+                ),
                 HoursWorkedGroup = ifelse(
                         HoursWorked < 15, "Minimal", ifelse(
                                 HoursWorked < 35, "PartTime", ifelse(
@@ -33,10 +52,16 @@ dat_adults <- dat_adults %>%
                                         )
                                 )
                         )
-                )
+                ),
+                HoursWorkedGroup = factor(HoursWorkedGroup, levels = c(
+                        "Minimal",
+                        "PartTime",
+                        "FullTime",
+                        "OverTime",
+                        "XOverTime"
+                )),
+                EarningsClassBinary = ifelse(EarningsClass == "<=50K", 0, 1)
         )
-
-dat_adults %>% head(3)
 
 dat_adults %>%
         ggplot(aes(x = EarningsClass)) +
@@ -49,17 +74,15 @@ dat_adults %>%
         geom_violin(aes(y = EducationNum), alpha = 0.05) +
         theme_moss()
 
-dat_adults %>%
-        select(
-                Age,
-                EducationNum,
-                CapitalGain,
-                CapitalLoss,
-                HoursWorked
-        ) %>%
-        ggpairs()
-
-dat_adults %>% head(3)
+# dat_adults %>%
+#         select(
+#                 Age,
+#                 EducationNum,
+#                 CapitalGain,
+#                 CapitalLoss,
+#                 HoursWorked
+#         ) %>%
+#         ggpairs()
 
 for (i in c(1,2,5,6,7,8,9,10,14,16)) {
 
@@ -87,20 +110,109 @@ for (i in c(1,2,5,6,7,8,9,10,14,16)) {
 
 }
 
-dat_adults_supertotals %>%
+plt_eda <- dat_adults_supertotals %>%
         ggplot(aes(x = ExplanatoryValues)) +
         geom_bar(aes(weight = TotalWorkers, group = Earnings, fill = Earnings), position = "dodge") +
         theme_moss() +
         theme(
-                panel.background = element_rect(fill = "white", color = "white"),
-                plot.background = element_rect(fill = "white"),
-                strip.background = element_rect(color = "white"),
-                axis.text.x = element_text(angle = 60, hjust = 1)
+                plot.title = element_blank(),
+                plot.subtitle = element_blank(),
+                axis.text.x = element_text(angle = 60, hjust = 1),
+                legend.position = "top",
+                legend.text = element_text(size = 18, face = "bold")
         ) +
         labs(
-
+                title = "Large Bucket EDA Visual",
+                subtitle = "Earnings by Various Metrics",
+                x = "",
+                y = "Total Workers",
+                caption = "Figure 1"
+        ) +
+        scale_fill_manual(
+                values = c(
+                        ">50K" = "#628395",
+                        "<=50K" = "#CF995F"
+                )
         ) +
         facet_wrap(. ~ ExplanatoryVariable, scales = "free", ncol = 1)
 
-ggsave("001-AdultEarnings/testimage.png", device = "png", width = 20, height = 30, units = "in")
+## ggsave(plt_eda, filename = "001-AdultEarnings/testimage.png", device = "png", width = 20, height = 30, units = "in")
+
+dat_adults_meanage <- dat_adults %>%
+        group_by(
+                EarningsClass
+        ) %>%
+        summarise(
+                MeanAge = mean(Age, na.rm = T)
+        )
+
+plt_age <- dat_adults %>%
+        ggplot(aes(x = EarningsClass)) +
+        geom_jitter(aes(y = Age, color = EarningsClass), width = 0.2, alpha = 0.1) +
+        geom_segment(data = dat_adults_meanage, aes(x = EarningsClass, xend = EarningsClass, y = 0, yend = MeanAge), color = "black", size = 1) +
+        geom_hline(aes(yintercept = 0), color = "black", size = 1) +
+        geom_point(data = dat_adults_meanage, aes(x = EarningsClass, y = MeanAge), color = "black", size = 10) +
+        geom_point(data = dat_adults_meanage, aes(x = EarningsClass, y = MeanAge), color = "white", size = 8) +
+        geom_text(data = dat_adults_meanage, aes(x = EarningsClass, y = MeanAge + 5, label = round(MeanAge, 1)), size = 5) +
+        theme_moss() +
+        theme(
+                panel.grid.major.y = element_line(size = 0.75, linetype = "dashed", color = "grey90")
+        ) +
+        labs(
+                x = "Earning Class",
+                y = "Age"
+        ) +
+        scale_y_continuous(
+                breaks = c(seq(0, 100, 10))
+        ) +
+        scale_color_manual(
+                values = c(
+                        ">50K" = "#628395",
+                        "<=50K" = "#CF995F"
+                )
+        )
+
+logisticmodel_age <- glm(
+        data = dat_adults,
+        family = "binomial",
+        formula = EarningsClassBinary ~ Age
+)
+
+logisticmodel_age %>% summary()
+
+logisticmodel_age_residuals <- data.frame(
+        Res = logisticmodel_age$residuals,
+        Rnk = c(1:length(logisticmodel_age$residuals))
+) %>%
+        ggplot() +
+        geom_point(aes(x = Rnk, y = Res), alpha = 0.1) +
+        geom_hline(aes(yintercept = 0), linetype = "dashed", size = 0.75, color = "firebrick1") +
+        theme_moss() +
+        theme(
+                axis.text.x = element_blank()
+        ) +
+        labs(
+                x = "",
+                y = "Residuals"
+        )
+
+logisticmodel_age_residuals2 <- data.frame(
+        Res = logisticmodel_age$residuals,
+        Rnk = c(1:length(logisticmodel_age$residuals))
+) %>%
+        ggplot() +
+        geom_histogram(aes(x = Res), bins = 100) +
+        theme_moss() +
+        theme(
+                #axis.text.x = element_blank()
+        ) +
+        labs(
+                x = "Residuals",
+                y = ""
+        )
+
+
+
+
+
 
